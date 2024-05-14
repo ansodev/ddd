3b614c081a06917c09061a6e07be2fd0ecc33a23
Anderson Floriano <anderson.floriano@gmail.com>
2024-05-14 10:57:59 -0300

    Fix tests

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -5,13 +5,13 @@ describe("Product unit tests", () => {
   it("should throw error when id is empty", () => {
     expect(() => {
       const product = new Product('', 'Product', 100);
-    }).toThrow('Id is required')
+    }).toThrow('Product: Id is required')
   })
 
   it("should throw error when name is empty", () => {
     expect(() => {
       const product = new Product('123', '', 100);
-    }).toThrow('Name is required')
+    }).toThrow('Product: Name is required');
   })
 
   it("should throw error when price is less than zero", () => {
@@ -19,6 +19,12 @@ describe("Product unit tests", () => {
       const product = new Product('123', 'Product', -2);
     }).toThrow('Price must be greater than zero')
   })
+
+  it('should acumulate errors', () => { 
+    expect(() => {
+      const product = new Product('', '', -2);
+    }).toThrow('Product: Id is required, Product: Name is required, Product: Price must be greater than zero')
+  });
 
   it("should change name", () => {
     const product = new Product('123', 'Product', 10);
